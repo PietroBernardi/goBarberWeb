2e868cf83ef06d0e1564de8431a607bef467409d
PietroBernardi <pietro.henrique.bernardi@gmail.com>
2020-03-10 22:09:03 -0300

    Sign In & Sign Up

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid Email Format')
+    .required('Email is Required'),
+  password: Yup.string()
+    .min(6, 'password must have at least 6 characters in length')
+    .required('Password is Required'),
+});
 
 export default function SignIn() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+
+        <button type="submit">Sign in</button>
+        <Link to="/register">Create Free Account</Link>
+      </Form>
+    </>
+  );
 }
