6277c9163a75d67eb21711df922a787790096118
PietroBernardi <pietro.henrique.bernardi@gmail.com>
2020-03-14 21:42:41 -0300

    Update profile and avatar

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+        <Input name="name" placeholder="Name" />
+        <Input type="email" name="email" placeholder="E-mail" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Old password" />
+        <Input type="password" name="password" placeholder="New password" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm new password"
+        />
+
+        <button type="submit">Update account</button>
+      </Form>
+      <button type="button" onClick={handleSignOut}>
+        Sign out
+      </button>
+    </Container>
+  );
 }
