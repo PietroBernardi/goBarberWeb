ebff950ffe35792860f4824214529d7119f132cf
PietroBernardi <pietro.henrique.bernardi@gmail.com>
2020-03-15 11:08:20 -0300

    Listing appointments

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 100%;
+  padding: 0.02px;
+  margin: 0px;
+
+  height: 100% auto;
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
 `;
